2f3f646dd27e8f5ef089a40a281205da588c1024
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2023-05-04 15:28:18 +0100

    create messages subdoc

--- backend/models/ticketModel.js
+++ backend/models/ticketModel.js
@@ -2,14 +2,23 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const messageSchema = new Schema({
+  message: { type: String, required: true },
+  user: { type: String, required: true },
+});
+
 const ticketSchema = new Schema(
   {
     title: { type: String, required: true },
     userId: { type: String, required: true },
     description: { type: String, required: true },
     active: { type: Boolean, required: true },
+    messages: [messageSchema],
   },
   { timestamps: true }
 );
 
-module.exports = mongoose.model('Ticket', ticketSchema);
+const Ticket = mongoose.model('Ticket', ticketSchema);
+const Message = mongoose.model('Message', messageSchema);
+
+module.exports = { Ticket, Message };
