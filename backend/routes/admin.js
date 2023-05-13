d043e5b7aeb155d2377a9269e37173de149e48bc
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2023-05-13 20:05:09 +0100

    admin signup/login

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { loginAdmin, signupAdmin } = require('../controllers/adminController');
 
 router.post('/login', loginAdmin);
-router.post('signup', signupAdmin);
+router.post('/signup', signupAdmin);
 
 module.exports = router;
